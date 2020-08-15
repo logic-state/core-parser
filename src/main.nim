5e74bf782a03a81eaa549c57e2b4e5e79d31b8bd
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-08-16 02:53:20 +0700

    fix: Accept input from stdout
    * Fix EOF error by using noise package
    
    * Save line history in repl mode

--- src/main.nim
+++ src/main.nim
@@ -1,25 +1,42 @@
 import graph, parser, generator, drawing
 
+
+proc print(fsm: StateDiagram) =
+  when defined(debug):
+    echo fsm
+
+  when defined(dot):
+    echo fsm.draw(Graphviz)
+  when defined(tsCode):
+    echo fsm.generate(into=TypeState,
+                    format=TypescriptCode)
+  when defined(jsCode):
+    echo fsm.generate(into=TypeState,
+                    format=JavascriptCode)
+  when defined(tsInterface):
+    echo fsm.generate(into=TypeState,
+                    format=TypescriptInterface)
+
+
 when isMainModule:
+  import noise, terminal, strutils
+
+  var repl = Noise.init()
+  repl.setPrompt("> ")
+
   var fsm = StateDiagram(diagram:
     when defined(graph): Multidigraph
-                            else: TransitionTable)
+    else: TransitionTable)
 
-  while true:
-    stderr.write "> "
-    doAssert fsm.parse(stdin.readLine()).ok
+  while isAtty(stdin) and repl.readLine():
+    let line = repl.getLine
+    discard fsm.parse(line)
 
-    when defined(debug):
-      echo fsm
+    fsm.print()
+    when promptHistory:
+      if line.len > 0: repl.historyAdd(line)
 
-    when defined(dot):
-      echo fsm.draw(Graphviz)
-    when defined(tsCode):
-      echo fsm.generate(into=TypeState,
-                      format=TypescriptCode)
-    when defined(jsCode):
-      echo fsm.generate(into=TypeState,
-                      format=JavascriptCode)
-    when defined(tsInterface):
-      echo fsm.generate(into=TypeState,
-                      format=TypescriptInterface)
+  if not isAtty(stdin):
+    for line in stdin.readAll.splitLines:
+      discard fsm.parse(line)
+    fsm.print()
