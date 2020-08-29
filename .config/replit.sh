c30931b5b475edd399804e7f13769c71317d67bd
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-08-30 04:51:35 +0700

    ci(demo): Add interactive prompt
    https://repl.it/@logicstate/logicstate#examples/detective.logic

--- .config/replit.sh
+++ .config/replit.sh
@@ -1,16 +1,51 @@
 #/bin/sh
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Install Inquirer.sh
+mkdir -p ../inquirer && wget https://github.com/kahkhang/Inquirer.sh/archive/master.tar.gz \
+-qO - | tar -xz -C ../inquirer --strip 1
+pushd ../inquirer ; ./build.py ; popd
+install-pkg bc && clear
+source ../inquirer/dist/list_input.sh
+
+codes=( 'Javascript' 'Typescript' 'Typescript Interface' )
+list_input "Which code to generate?" codes sel_code
+
+if [ "$sel_code" != 'Typescript Interface' ]; then
+  impls=( 'Type State' 'State Pattern' )
+  list_input "And the state machine going to be implemented as ..." impls sel_impl
+fi
+
+case $sel_code in
+  'Typescript Interface') fmt=tsInterface ; hl=ts ;;
+  'Typescript') fmt=tsCode ; hl=ts ;;
+  'Javascript') fmt=jsCode ; hl=js ;;
+esac
+
+case $sel_impl in
+  'Type State') impl=typestate ;;
+  'State Pattern') impl=statepattern ;;
+esac
+
+echo '===================================='
+echo 'running examples/detective.logic'
+echo -e "        $YELLOW^^^^^^^^^^^^^^^^^^^^^^^^ open that, pelase 🥺$NC"
+echo 'PLEASE WAIT...'
+echo 'we are building the compiler 😂'
+echo '===================================='
 
 # Install tools for live interactive
 install-pkg graphviz && dot -c
 wget https://github.com/watchexec/watchexec/releases/download/1.14.0/watchexec-1.14.0-x86_64-unknown-linux-gnu.tar.xz \
--O - | tar -xJ -C .. --strip 1 --no-anchored 'watchexec'
+-qO - | tar -xJ -C .. --strip 1 --no-anchored 'watchexec'
 wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-gnu.tar.gz \
--O - | tar -xz -C .. --strip 1 --no-anchored 'bat'
+-qO - | tar -xz -C .. --strip 1 --no-anchored 'bat'
 
 # Compile 2 version of logicstate
-nimble -d:tsInterface -o:'../lost-code' -y compile src/main.nim
+nimble -d:$fmt -d:$impl -o:'../lost-code' -y compile src/main.nim
 nimble -d:dot -o:'../lost-diagram' -y compile src/main.nim
 
-# Live reload
+## Live reload
 ../watchexec -c -w examples -e logic 'cat examples/detective.logic | ../lost-diagram | dot -Tsvg > examples/detective.svg' &
-../watchexec -c -w examples -e logic 'cat examples/detective.logic | ../lost-code | ../bat -n -l ts'
+../watchexec -c -w examples -e logic "cat examples/detective.logic | ../lost-code | ../bat -p -l $hl"
