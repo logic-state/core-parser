cb212166859b32e7fadc6d12aa9f9420ec38beab
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-08-30 23:56:47 +0700

    ci(demo): Skip installing system dependencies if already available

--- .config/replit.sh
+++ .config/replit.sh
@@ -2,13 +2,17 @@
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-# Install Inquirer.sh
-mkdir -p ../inquirer && wget https://github.com/kahkhang/Inquirer.sh/archive/master.tar.gz \
--qO - | tar -xz -C ../inquirer --strip 1
-pushd ../inquirer ; ./build.py ; popd
-install-pkg bc && clear
+# Install tool for interactive prompt
+if ! [ -d ../inquirer ]; then
+  mkdir ../inquirer && wget https://github.com/kahkhang/Inquirer.sh/archive/master.tar.gz \
+  -qO - | tar -xz -C ../inquirer --strip 1
+  pushd ../inquirer ; ./build.py ; popd
+fi
+[ -x "$(command -v bc)" ] || install-pkg bc && clear
 source ../inquirer/dist/list_input.sh
 
+
+# Interactive prompts
 codes=( 'Javascript' 'Typescript' 'Typescript Interface' )
 list_input "Which code to generate?" codes sel_code
 
@@ -18,7 +22,7 @@ if [ "$sel_code" != 'Typescript Interface' ]; then
 fi
 
 case $sel_code in
-  'Typescript Interface') fmt=tsInterface ; hl=ts ;;
+  'Typescript Interface') fmt=tsInterface ; hl=ts ; impl=typestate ;;
   'Typescript') fmt=tsCode ; hl=ts ;;
   'Javascript') fmt=jsCode ; hl=js ;;
 esac
@@ -35,17 +39,24 @@ echo 'PLEASE WAIT...'
 echo 'we are building the compiler 😂'
 echo '===================================='
 
+
 # Install tools for live interactive
-install-pkg graphviz && dot -c
-wget https://github.com/watchexec/watchexec/releases/download/1.14.0/watchexec-1.14.0-x86_64-unknown-linux-gnu.tar.xz \
+[ -x "$(command -v dot)" ] || install-pkg graphviz && dot -c
+
+[ -f ../watchexec ] || wget \
+https://github.com/watchexec/watchexec/releases/download/1.14.0/watchexec-1.14.0-x86_64-unknown-linux-gnu.tar.xz \
 -qO - | tar -xJ -C .. --strip 1 --no-anchored 'watchexec'
-wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-gnu.tar.gz \
+
+[ -f ../bat ] || wget \
+https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-gnu.tar.gz \
 -qO - | tar -xz -C .. --strip 1 --no-anchored 'bat'
+
 
 # Compile 2 version of logicstate
 nimble -d:$fmt -d:$impl -o:'../lost-code' -y compile src/main.nim
-nimble -d:dot -o:'../lost-diagram' -y compile src/main.nim
+[ -f ../lost-diagram ] || nimble -d:dot -o:'../lost-diagram' -y compile src/main.nim
 
-## Live reload
+
+# Live reload
 ../watchexec -c -w examples -e logic 'cat examples/detective.logic | ../lost-diagram | dot -Tsvg > examples/detective.svg' &
 ../watchexec -c -w examples -e logic "cat examples/detective.logic | ../lost-code | ../bat -p -l $hl"
